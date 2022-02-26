ae59e9d346f64fea304c9e2912df60272cb428cb
Burak Özkan <burak.ozkan@path.com.tr>
2022-02-26 18:34:26 +0300

    create planet class

--- main.py
+++ main.py
@@ -9,6 +9,33 @@ pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
 
+class Planet:
+    
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 250 / AU #1AU = 100 pixels
+    TIMESTEP = 3600 * 24 # 1 day
+    
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+        
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+        
+        self.x_vel = 0
+        self.y_vel = 0
+        
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.color, (x,y), self.radius)
+        
+
 def main():
     run = True
     clock = pygame.time.Clock()
