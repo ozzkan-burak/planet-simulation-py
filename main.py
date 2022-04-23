8640d94fe9b6046a0545b3ac367ea30753ba5bfc
Burak ÖZKAN <burak.ozkan@flo.com.tr>
2022-04-23 13:00:27 +0300

    episode4

--- main.py
+++ main.py
@@ -37,6 +37,17 @@ class Planet:
     def draw(self, win):
         x = self.x * self.SCALE + WIDTH / 2
         y = self.y * self.SCALE + HEIGHT / 2
+        
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                x,y = point
+                x = x * self.SCALE + WIDTH / 2
+                y = y * self.SCALE + HEIGHT / 2
+                updated_points.append((x, y))
+                
+            pygame.draw.lines(win, self.color, False, updated_points, 2)
+        
         pygame.draw.circle(win, self.color, (x,y), self.radius)
         
     def attraction(self, other):
